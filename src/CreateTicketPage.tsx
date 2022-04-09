482bb59fa12280beb59714fc8cb6304225811d73
tjcichra <8120075+tjcichra@users.noreply.github.com>
2022-04-09 16:02:33 -0400

    create ticket page

--- src/CreateTicketPage.tsx
+++ src/CreateTicketPage.tsx
@@ -1,24 +1,42 @@
-import { Grid, Paper, TextField, Typography } from "@mui/material";
-import { Editor, EditorState } from 'draft-js';
-import { useState, useRef, useEffect } from "react";
+import { Box, Button, Grid, Paper, TextField, Typography } from "@mui/material";
+import { MouseEvent, useState, useRef, useEffect, useMemo } from "react";
+import * as ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import { useNavigate } from "react-router-dom";
+import { API_BASE_URL } from "./EnvironmentVariables";
 
 export default function CreateTicketPage() {
+    const [subject, setSubject] = useState('');
+    const [description, setDescription] = useState('');
+    const [priority, setPriority] = useState('');
 
-    const [editorState, setEditorState] = useState(
-        EditorState.createEmpty()
-    );
+    let navigate = useNavigate();
 
-    const editor = useRef<Editor>(null);
-
-    function focusEditor() {
-        if(editor.current) {
-            editor.current.focus();
-        }
+    const handleChange = (description: string) => {
+        setDescription(description);
     }
 
-    useEffect(() => {
-        focusEditor()
-    }, []);
+    const handleSubmit = (event: MouseEvent<HTMLButtonElement>) => {
+        const ticket = {
+            subject,
+            description,
+            status: 'OPEN',
+            priority
+        };
+
+        fetch(`${API_BASE_URL}/tickets`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(ticket)
+        })
+        .then(response => {
+            if(response.ok) {
+                navigate('/')
+            }
+        });
+    }
 
     return (
         <Grid>
@@ -32,12 +50,34 @@ export default function CreateTicketPage() {
                 <Typography component="h2" variant="h6" color="primary" gutterBottom>
                     Create new ticket
                 </Typography>
-                <Editor
-                    ref={editor}
-                    editorState={editorState}
-                    onChange={editorState => setEditorState(editorState)}
-                />
                 <TextField id="outlined-basic" label="Subject" variant="outlined" />
+                <Box sx={{ mt: 2 }}>
+                    <ReactQuill
+                        value={description}
+                        onChange={handleChange}
+                        modules={{
+                            toolbar: [
+                                [{ 'header': [1, 2, false] }],
+                                ['bold', 'italic', 'underline','strike', 'blockquote'],
+                                [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+                                ['link', 'image'],
+                                ['clean']
+                            ],
+                        }}
+                    />
+                </Box>
+                <TextField margin="normal" id="outlined-basic" label="Priority" variant="outlined" />
+                <Button
+                    onClick={handleSubmit}
+                    variant="contained"
+                    color='primary'
+                    sx={{
+                        mt: 2,
+                    }}
+                    fullWidth
+                >
+                    Create Ticket
+                </Button>
             </Paper>
         </Grid>
     )
